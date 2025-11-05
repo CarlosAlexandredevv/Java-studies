130276851bfa22c987b166fd21bbec460a9a0078
Carlos Alexandre <alexfilho062005@gmail.com>
2025-11-05 13:30:14 -0300

    :sparkles: variables and constants

---
+++ essencial/src/fundamentals/VariablesAndConstants.java
@@ -0,0 +1,7 @@
+public class VariablesAndConstants {
+    public static void main(String[] args) {
+        int age = 20;
+        final String name = "Carlos Alexandre";
+        System.out.println("My name is " + name + " and I am " + age + " years old");
+    }
+}
