e0b7bf4f32c3f6576ddd41a041a2aeb92e613eb2
Carlos Alexandre <alexfilho062005@gmail.com>
2025-11-05 14:05:58 -0300

    :sparkles: types primitives

---
+++ essencial/src/fundamentals/PrimitivesTypes.java
@@ -0,0 +1,26 @@
+public class PrimitivesTypes {
+    public static void main(String[] args) {
+        char symbol = '@';
+        
+        boolean itRaining = false;
+        boolean iAmHungry = true;
+
+        byte number1 = 127;
+        short number2 = 32767;
+        int number3 = 2147483647;
+        long number4 = 123;
+        float f1 = 123.45f;
+        double f2 = 123.45;
+        
+        System.out.println(symbol);
+        System.out.println(itRaining);
+        System.out.println(iAmHungry);
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(symbol);
+        System.out.println(f1);
+        System.out.println(f2);
+    }
+}
