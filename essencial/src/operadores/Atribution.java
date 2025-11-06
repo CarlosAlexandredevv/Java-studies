082837c64a1256c1f83ec63288a8add90f18a37f
Carlos Alexandre <alexfilho062005@gmail.com>
2025-11-06 10:04:36 -0300

    :sparkles: atribution operation

---
+++ essencial/src/operadores/Atribution.java
@@ -0,0 +1,13 @@
+public class Atribution {
+    
+    public static void main(String[] args) {
+        int a = 7;
+        a += 3;
+        a -= 2;
+        a *= 3;
+        a /= 2;
+        a %= 3;
+
+        System.out.println(a);
+    }
+}
