f36c9f31cfc74628df211560577f4ceb126d13d5
Carlos Alexandre <alexfilho062005@gmail.com>
2025-11-06 09:56:14 -0300

    :sparkles: operation relational

---
+++ essencial/src/operadores/Relational.java
@@ -0,0 +1,20 @@
+public class Relational {
+    
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+        System.out.println(a == 7);
+        System.out.println(a == b);
+
+        System.out.println(a != 7);
+        System.out.println(a != b);
+
+        System.out.println(7 == '7');
+    }
+}
