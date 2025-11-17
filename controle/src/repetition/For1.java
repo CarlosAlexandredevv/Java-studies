a61f5ad7d838c5d83b41e8c9b1935c2f92ff6116
Carlos Alexandre <alexfilho062005@gmail.com>
2025-11-17 09:48:47 -0300

    for

---
+++ controle/src/repetition/For1.java
@@ -0,0 +1,11 @@
+package repetition;
+
+public class For1 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i += 1){
+            System.out.println(i);
+        }
+
+        System.out.println("Ending...");
+    }
+}
---
+++ controle/src/repetition/For2.java
@@ -0,0 +1,9 @@
+package repetition;
+
+public class For2 {
+    public static void main(String[] args) {
+       for(int i = 10; i >= 1; i-= 2){
+        System.out.println(i);
+       }
+    }
+}
